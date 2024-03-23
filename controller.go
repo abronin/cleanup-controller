d1206f22643e560cbe7485124311387b8ec432df
Andrey Bronin <andrey.bronin@uptempo.io>
2024-03-22 19:47:49 -0700

    Test gh actions

---
+++ controller.go
@@ -0,0 +1,296 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/time/rate"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+)
+
+const controllerAgentName = "namespace-cleanup-controller"
+
+const (
+	// SuccessSynced is used as part of the Event 'reason' when a Namespace is deleted
+	SuccessDeleted = "Deleted"
+
+	// MessageNamespaceDeleted is the message used for an Event fired when a
+	// Namespace is deleted successfully
+	MessageNamespaceDeleted = "Namespace deleted successfully"
+)
+
+// Controller is the controller implementation for Foo resources
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+
+	namespacesLister corelisters.NamespaceLister
+	namespacesSynced cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
+
+// NewController returns a new sample controller
+func NewController(ctx context.Context, kubeclientset kubernetes.Interface, namespaceInformer coreinformers.NamespaceInformer) *Controller {
+	logger := klog.FromContext(ctx)
+
+	// logged for sample-controller types.
+	logger.V(4).Info("Creating event broadcaster")
+
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartStructuredLogging(0)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+	ratelimiter := workqueue.NewMaxOfRateLimiter(
+		workqueue.NewItemExponentialFailureRateLimiter(5*time.Millisecond, 1000*time.Second),
+		&workqueue.BucketRateLimiter{Limiter: rate.NewLimiter(rate.Limit(50), 300)},
+	)
+
+	controller := &Controller{
+		kubeclientset:    kubeclientset,
+		namespacesLister: namespaceInformer.Lister(),
+		namespacesSynced: namespaceInformer.Informer().HasSynced,
+		workqueue:        workqueue.NewRateLimitingQueue(ratelimiter),
+		recorder:         recorder,
+	}
+
+	logger.Info("Setting up event handler")
+	// Set up an event handler for when Namespace resources change
+	namespaceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.handleObject,
+		UpdateFunc: func(old, new interface{}) {
+			newNs := new.(*corev1.Namespace)
+			// oldNs := old.(*corev1.Namespace)
+			if _, ok := newNs.GetAnnotations()["namespace-cleanup-controller/delete-at"]; newNs.Status.Phase == "Terminating" || !ok {
+				// Periodic resync will send update events for all known Namespaces.
+				// Two different versions of the same Namespace will always have different RVs.
+				// Also we are not interested if new version is already in Terminating status.
+				return
+			}
+			controller.handleObject(new)
+		},
+		// DeleteFunc: controller.handleObject,
+	})
+
+	return controller
+}
+
+// Run will set up the event handlers for types we are interested in, as well
+// as syncing informer caches and starting workers. It will block until stopCh
+// is closed, at which point it will shutdown the workqueue and wait for
+// workers to finish processing their current work items.
+func (c *Controller) Run(ctx context.Context, workers int) error {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+	logger := klog.FromContext(ctx)
+
+	// Wait for the caches to be synced before starting workers
+	logger.Info("Waiting for informer caches to sync")
+
+	if ok := cache.WaitForCacheSync(ctx.Done(), c.namespacesSynced); !ok {
+		return fmt.Errorf("failed to wait for caches to sync")
+	}
+
+	logger.Info("Starting workers", "count", workers)
+	// Launch two workers to process Namespace resources
+	for i := 0; i < workers; i++ {
+		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
+	}
+
+	logger.Info("Started workers")
+	<-ctx.Done()
+	logger.Info("Shutting down workers")
+
+	return nil
+}
+
+// runWorker is a long-running function that will continually call the
+// processNextWorkItem function in order to read and process a message on the
+// workqueue.
+func (c *Controller) runWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+// processNextWorkItem will read a single work item off the workqueue and
+// attempt to process it, by calling the deleteHandler.
+func (c *Controller) processNextWorkItem(ctx context.Context) bool {
+	obj, shutdown := c.workqueue.Get()
+	logger := klog.FromContext(ctx)
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.workqueue.Forget(obj)
+			utilruntime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+
+		// Run the deleteHandler, passing it the namespace/name string of the
+		// Namespace resource to be deleted.
+		if err := c.deleteHandler(ctx, key); err != nil {
+			// Put the item back on the workqueue to handle any transient errors.
+			c.workqueue.AddRateLimited(key)
+			return fmt.Errorf("error deleting '%s': %s, requeuing", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.workqueue.Forget(obj)
+		logger.Info("Successfully deleted", "resourceName", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		utilruntime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+// deleteHandler deletes the namespace
+// with the current status of the resource.
+func (c *Controller) deleteHandler(ctx context.Context, key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	logger := klog.LoggerWithValues(klog.FromContext(ctx), "resourceName", key)
+
+	_, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	// Get the Namespace resource with this namespace/name
+	namespace, err := c.namespacesLister.Get(name)
+	if err != nil {
+		// The Namespace resource may no longer exist, in which case we stop
+		// processing.
+		if errors.IsNotFound(err) {
+			utilruntime.HandleError(fmt.Errorf("namespace '%s' in work queue no longer exists", key))
+			return nil
+		}
+
+		return err
+	}
+
+	logger.V(4).Info("Delete namespace resource", "namespace", name)
+	err = c.kubeclientset.CoreV1().Namespaces().Delete(ctx, name, metav1.DeleteOptions{})
+	if err != nil {
+		return err
+	}
+
+	c.recorder.Event(namespace, corev1.EventTypeNormal, SuccessDeleted, MessageNamespaceDeleted)
+	return nil
+}
+
+// enqueueNamespace takes a Namespace resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Namespace.
+func (c *Controller) enqueueNamespace(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		utilruntime.HandleError(err)
+		return
+	}
+	c.workqueue.Add(key)
+}
+
+// handleObject will take any resource implementing metav1.Object and check
+// if it has a delete-at label. It then enqueues that resource will be processed.
+func (c *Controller) handleObject(obj interface{}) {
+	var object metav1.Object
+	var ok bool
+	logger := klog.FromContext(context.Background())
+	if object, ok = obj.(metav1.Object); !ok {
+		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("error decoding object, invalid type"))
+			return
+		}
+		object, ok = tombstone.Obj.(metav1.Object)
+		if !ok {
+			utilruntime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
+			return
+		}
+		logger.V(4).Info("Recovered deleted object", "resourceName", object.GetName())
+	}
+
+	logger.V(4).Info("Processing object", "object", klog.KObj(object))
+
+	if val, ok := object.GetAnnotations()["namespace-cleanup-controller/delete-at"]; ok {
+		logger.V(4).Info("Found namespace-cleanup-controller/delete-at annotation", "namespace", object.GetName())
+
+		deleteAt, err := time.Parse(time.RFC3339, val)
+		if err != nil {
+			utilruntime.HandleError(fmt.Errorf("error decoding namespace-cleanup-controller/delete-at value for namespace, should be time.RFC3339"))
+			return
+		}
+		if deleteAt.Before(time.Now()) {
+			logger.Info("Sending namespace for deletion", "namespace", object.GetName())
+			c.enqueueNamespace(object)
+		} else {
+			logger.V(4).Info("Skipping namespace because it is too early to delete", "namespace", object.GetName(), "delete-at", val)
+		}
+	}
+}
